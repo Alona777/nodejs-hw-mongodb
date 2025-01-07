09455dda02bcceb2b516644d28539754a9f469d1
Alona Adamska <alona.adamska@gmail.com>
2025-01-07 19:15:08 +0100

    swagger fix

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -1,14 +1,15 @@
-import swaggerUi from 'swagger-ui-express';
-import { createRequire } from 'module';
+import createHttpError from 'http-errors';
+import swaggerUI from 'swagger-ui-express';
+import fs from 'node:fs';
 
-// Используем require для импорта JSON
-const require = createRequire(import.meta.url);
-const swaggerDocument = require('../../docs/swagger.json');
+import { SWAGGER_PATH } from '../constants/index.js';
 
 export const swaggerDocs = () => {
-  return swaggerUi.serveFiles(
-    swaggerDocument,
-    {},
-    swaggerUi.setup(swaggerDocument)
-  );
+  try {
+    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
+    return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
+  } catch {
+    return (req, res, next) =>
+      next(createHttpError(500, "Can't load swagger docs"));
+  }
 };
